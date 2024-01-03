e75ca6602b4ae670382b4775c9eb7bf480f6fd73
Donatas Adomavicius <adom.donatas@gmail.com>
2024-01-03 20:36:54 +0800

    Feat: add toast notifications globally

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,15 @@
     class="min-w-full min-h-screen font-sans text-sm antialiased bg-gray-100 text-center"
   >
     <router-view />
+    <ToastContainer />
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { provide } from 'vue'
+import useToast from './composables/toast'
+import ToastContainer from './components/notification/ToastContainer.vue'
+
+const toast = useToast()
+provide('toast', toast)
+</script>
